fb0d197b79cdfaa2fcbd79392b2fb9d0978277f4
nguyen the anh <anhnt20090116@gmail.com>
2022-10-02 00:53:34 +0700

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/crop_image_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.crop_image_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/crop_image.dart
@@ -0,0 +1,2 @@
+export 'package:crop_image_flutter/src/crop_controller.dart';
+export 'package:crop_image_flutter/src/crop_image.dart';
